b08fe5543556b75ca675ccac7fb23f26ad893151
Csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-08-30 00:47:09 -0500

    estructura del proyecto

---
+++ src/test/java/edu/escuelaing/eci/arsw/gooffer/GoofferApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.eci.arsw.gooffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoofferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
