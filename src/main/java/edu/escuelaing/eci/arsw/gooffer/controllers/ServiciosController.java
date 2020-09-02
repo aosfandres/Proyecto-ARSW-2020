49fe99689442ed863f46b8e1d011cc358d89f678
Csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-09-02 16:03:47 -0500

    Agregando html

--- src/main/java/edu/escuelaing/eci/arsw/gooffer/controllers/ServiciosController.java
+++ src/main/java/edu/escuelaing/eci/arsw/gooffer/controllers/ServiciosController.java
@@ -1,5 +1,17 @@
 package edu.escuelaing.eci.arsw.gooffer.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ServiciosController {
+
+	@GetMapping("/example")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "example";
+	}
 
 }
